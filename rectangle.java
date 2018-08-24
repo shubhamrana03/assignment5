a8d0ec54392780675363e2659ccea4cfb1f7aaa6
shubhamrana03 <42331840+shubhamrana03@users.noreply.github.com>
2018-08-24 09:59:51 +0530

    Update rectangle.java

--- rectangle.java
+++ rectangle.java
@@ -5,7 +5,7 @@ public class rectangle
  Scanner in = new Scanner(System.in);
  double a,b;
  double c;
- System.out.println("Enter both the sides of triangle");
+ System.out.println("Enter both the sides of rectangle");
  a = in.nextDouble();
  b = in.nextDouble();
  c= a*b;
